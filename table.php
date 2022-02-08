835ddcbb3ccdc861f7e6d30b0c409d94af40a033
57053aison <97937654+57053aison@users.noreply.github.com>
2022-02-08 14:37:46 +0700

    Update table.php

--- table.php
+++ table.php
@@ -5,9 +5,15 @@
   <body>
     <table border="1">
       <tr>
-        <th>คอลัมน์ที่ 1</th>
-        <td>คอลัมน์ที่ 2</td>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
       </tr>
-    </table>
-  </body> 
-</html>
+      <tr>
+        <td>1</td>
+        <td>ใอสนป์ ศรีธนัชโกสิณ</td>
+        <td>ฟ้าใส</td>
+        <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
+        <td><a href="https://twitter.com/sjk_kith?lang=en">twitter</a></td>
