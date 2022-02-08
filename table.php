58c7a09483ff1a08524a336fb89b6c20b2f5c299
57053aison <97937654+57053aison@users.noreply.github.com>
2022-02-08 15:33:10 +0700

    Update table.php

--- table.php
+++ table.php
@@ -38,6 +38,20 @@
         <td>อารีอานา</td>
         <td><img src="https://assets.teenvogue.com/photos/613b5fd248eda7f19679403c/2:3/w_1344,h_2017,c_limit/1235152164" width="130px"></td>
         <td><a href="https://twitter.com/arianatoday?lang=en">twitter</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>อารี สเตแพรนส์ เลฟฟ์</td>
+        <td>เลาฟ์</td>
+        <td><img src="https://lastfm.freetls.fastly.net/i/u/770x0/114ff5c03bbc402ac0b14a20a30c8afd.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/lauvsongs?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td>โคนัน ลี เกรย์</td>
+        <td>โคนัน</td>
+        <td><img src="https://yt3.ggpht.com/gMO0WsckiQPI6NlWKCq51VJeMxY7GWiRkBgkyPalySBVKI41LR6-Dehlj8WDm6KHY1h07C1-Puc=s900-c-k-c0x00ffffff-no-rj" width="130px"></td>
+        <td><a href="https://twitter.com/conangray?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
       </tr>  
     </table>
   </body> 
