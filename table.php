1a431c85119743f3efa037c16fddd3e8f05ef8a5
57053aison <97937654+57053aison@users.noreply.github.com>
2022-02-08 14:53:57 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,7 +15,7 @@
         <td>1</td>
         <td>ส้ม</td>
         <td>มารี เออเจนี เลอเอย์</td>
-        <td><img src="https://lofficielthailand.com/wp-content/uploads/2020/09/Zommarie-Bubble-1-scaled.jpg" width="130px"></td>
+        <td><img src="https://s.isanook.com/jo/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2pvLzAvdWQvNDg0LzI0MjA4MTMvejIuanBn.jpg" width="130px"></td>
         <td><a href="https://twitter.com/zommarie?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
       </tr>  
     </table>
