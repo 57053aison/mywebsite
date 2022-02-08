64ceb6c910e2c684b72b634ad160b6e62369e69b
57053aison <97937654+57053aison@users.noreply.github.com>
2022-02-08 14:51:57 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,7 +13,7 @@
       </tr>
       <tr>
         <td>1</td>
-        <tdส้ม></td>
+        <td>ส้ม</td>
         <td>มารี เออเจนี เลอเอย์</td>
         <td><img src="https://lofficielthailand.com/wp-content/uploads/2020/09/Zommarie-Bubble-1-scaled.jpg" width="130px"></td>
         <td><a href="https://twitter.com/zommarie?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
