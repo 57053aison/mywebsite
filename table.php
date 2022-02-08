fa2dff949c2515641757dc2648de6fc01b015865
57053aison <97937654+57053aison@users.noreply.github.com>
2022-02-08 15:18:05 +0700

    Update table.php

--- table.php
+++ table.php
@@ -24,6 +24,20 @@
         <td>โอลิเวีย</td>
         <td><img src="https://assets.teenvogue.com/photos/6076dcb99774944e1309a4d1/2:3/w_1000,h_1500,c_limit/olivia.jpeg" width="130px"></td>
         <td><a href="https://twitter.com/oliviarodrigo?lang=en">twitter</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>กฤตย์ จีรพัฒนานุวงศ์</td>
+        <td>กิต</td>
+        <td><img src="https://scontent.fbkk9-3.fna.fbcdn.net/v/t1.6435-9/188460119_331609828622408_1402242326305635154_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGNr02FoW9HAWbWl6cEYeMxKBpNId4sVpkoGk0h3ixWmRLIJly6a5YR_9GincMV49U9p8dlvhpLjsJfBO963GAp&_nc_ohc=zfM1fdxiFo0AX-YraGP&_nc_ht=scontent.fbkk9-3.fna&oh=00_AT_LAlweG7_xfy_mfz9zqGZ8-1fsqLonrwQoTK3eVjvJpQ&oe=6225ECD8" width="130px"></td>
+        <td><a href="https://twitter.com/kittyumbs?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>อารีอานา กรานเด-บูเทรา</td>
+        <td>อารีอานา</td>
+        <td><img src="https://assets.teenvogue.com/photos/613b5fd248eda7f19679403c/2:3/w_1344,h_2017,c_limit/1235152164" width="130px"></td>
+        <td><a href="https://twitter.com/arianatoday?lang=en">twitter</a></td>
       </tr>  
     </table>
   </body> 
