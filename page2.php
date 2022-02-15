4e02b5aec89b087e742ee36839dcb7ce246ce4a2
57053aison <97937654+57053aison@users.noreply.github.com>
2022-02-15 13:34:10 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -2,16 +2,16 @@
 	<head>
 		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
 		<style>
-			h1{
-				color: green;
-			  }
-			h2{
-				color : yellow;
-			  }
+			#text1{
+				color:green;
+			}
+			#text2{
+				color:yellow;
+			}
 		</style>
 	</head>
 	<body>
-		<p style="color:green;">ข้อความนี้สีเขียวปกติ</p>
-		<p style="color:#82933f;">ข้อความนี้สีเขียวรหัสสี</p>
+		<h1 id="text1"> ข้อความนี้สีเขียว H1 </h1>
+		<h1 id="text2"> ข้อความนี้สีเหลือง H1 </h1>
 	</body>
 </html>
