4fcb08fcdcad255f0a412caaf1ee5027fb32be35
57053aison <97937654+57053aison@users.noreply.github.com>
2022-02-22 13:41:43 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(MiPancake.woff)
+    }
+     @font-face{
+      font-family: myFont2;
+      src: url(SP-Yok N.woff)
+    }
+    @font1{
+      font-family: myFont1;
+    }
+    @font1{
+      font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวใอสนป์ ศรีธนัชโกสิณ ม.4/13 เลขที่ 25 เลขประจำตัว 57053</h1>
+  <h1 id="font2">นางสาวใอสนป์ ศรีธนัชโกสิณ ม.4/13 เลขที่ 25 เลขประจำตัว 57053</h1>
+</body>
+</html>
